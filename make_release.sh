27faff3ef6343032564ede55f2e03886c48282db
François Kooman <fkooman@tuxed.net>
2013-07-12 11:07:19 +0200

    update make_release script

--- make_release.sh
+++ make_release.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+RELEASE_DIR=${HOME}/Releases
+GITHUB_USER=fkooman
+PROJECT_NAME=ssp-voot-groups
+
 if [ -z "$1" ]
 then
 
@@ -12,30 +16,66 @@ Examples:
     sh make_release.sh master
     sh make_release.sh develop
 
+If you want to GPG sign the release, you can specify the "sign" parameter, this will
+invoke the gpg command line tool to sign it.
+
+   sh make_release 0.1.0 sign
+
 EOF
 exit 1
 else
     TAG=$1
 fi
 
+mkdir -p ${RELEASE_DIR}
+rm -rf ${RELEASE_DIR}/${PROJECT_NAME}
 
 # get Composer
 (
-cd /tmp
+cd ${RELEASE_DIR}
 curl -O http://getcomposer.org/composer.phar
 )
 
 # clone the tag
-
 (
-cd /tmp
-git clone -b $TAG https://github.com/fkooman/ssp-voot-groups.git
-cd ssp-voot-groups
-php /tmp/composer.phar install
-rm composer.json composer.lock
-rm -rf .git
-rm -f .gitignore
-cd ..
-tar -czf ssp-voot-groups-$TAG.tar.gz ssp-voot-groups
-rm -rf ssp-voot-groups
+cd ${RELEASE_DIR}
+git clone -b ${TAG} https://github.com/${GITHUB_USER}/${PROJECT_NAME}.git
+)
+
+# run Composer
+(
+cd ${RELEASE_DIR}/${PROJECT_NAME}
+php ${RELEASE_DIR}/composer.phar install
+)
+
+# remove Git and Composer files
+(
+rm -rf ${RELEASE_DIR}/${PROJECT_NAME}/.git
+rm -f ${RELEASE_DIR}/${PROJECT_NAME}/.gitignore
+rm -f ${RELEASE_DIR}/${PROJECT_NAME}/composer.json
+rm -f ${RELEASE_DIR}/${PROJECT_NAME}/composer.lock
+)
+
+# create tarball
+(
+cd ${RELEASE_DIR}
+tar -czf ${PROJECT_NAME}-${TAG}.tar.gz ${PROJECT_NAME}
+)
+
+# create checksum file
+(
+cd ${RELEASE_DIR}
+shasum ${PROJECT_NAME}-${TAG}.tar.gz > ${PROJECT_NAME}.sha
+)
+
+# sign it if requested
+(
+if [ -n "$2" ]
+then
+	if [ "$2" == "sign" ]
+	then
+		cd ${RELEASE_DIR}
+		gpg -o ${PROJECT_NAME}.sha.gpg  --clearsign ${PROJECT_NAME}.sha
+	fi
+fi
 )
